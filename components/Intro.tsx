a95995284afb80640b47f329b8b5a17629152503
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2023-11-02 22:48:54 -0700

    Created Setup for Portfolio

---
+++ components/Intro.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import profileImg from "@/public/JaedonSpurlock.png";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={profileImg}
+              alt="Jaedon Portrait"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 text-3xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            💻
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-light !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        {
+          "Hello, I'm Jaedon Spurlock, a full-time student from Cal State San Marcos studying Software Engineering. I enjoy making web and desktop applications."
+        }
+      </motion.p>
+    </section>
+  );
+}
