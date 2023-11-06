ccb735d8599f1568780fb02f011a49dab784d2d2
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2023-11-06 14:27:36 -0800

    fixed overlap issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,14 @@ export default function RootLayout({
           "${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90"
         }
       >
-        <div className="bg-[#E3F4F4] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#a9b9b9]"></div>
-        <div className="bg-[#FCF5ED] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#cec7c0]"></div>
+        <div
+          style={{ transform: "translate3d(0,0,0)" }}
+          className="bg-[#E3F4F4] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#a9b9b9]"
+        ></div>
+        <div
+          style={{ transform: "translate3d(0,0,0)" }}
+          className="bg-[#FCF5ED] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#cec7c0]"
+        ></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
